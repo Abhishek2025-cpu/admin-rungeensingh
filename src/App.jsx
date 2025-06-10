e7e48497e17482baff6d009fdabbb01568c3c0f3
Abhinavsaini121 <abhinavsaini@pearlorganisation.com>
2025-06-10 15:04:40 +0530

    change folders structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,14 @@ import Sidebar from './components/Sidebar';
 // Pages
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
-import AddInventory from './pages/AddInventory';
-import InventoryList from './pages/InventoryList';
-import Orders from './pages/Orders';
-import PurchaseHistory from './pages/PurchaseHistory';
+// import AddInventory from './pages/AddInventory';
+// import InventoryList from './pages/InventoryList';
+// import Orders from './pages/Orders';
+import PurchaseHistory from './pages/Purchase history/PurchaseHistory';
 
 // Category Pages
-import AddCategory from './pages/category/AddCategory';
-import ViewCategory from './pages/category/ViewCategory';
-
+// import AddCategory from './pages/category/AddCategory';
+// import ViewCategory from './pages/category/ViewCategory';
 
 function App() {
   return (
@@ -22,9 +21,9 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Dashboard />}>
-          <Route path="add-inventory" element={<AddInventory />} />
+          {/* <Route path="add-inventory" element={<AddInventory />} />
           <Route path="inventory-list" element={<InventoryList />} />
-          <Route path="purchase-history" element={<Orders />} />
+          <Route path="purchase-history" element={<Orders />} /> */}
         </Route>
       </Routes>
     </Router>
