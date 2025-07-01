0cd82dbc1fb69b3f582b7af15c41bb87c62532dd
Sujata-Pearl <sujata@pearlorganisation.com>
2025-07-01 18:36:53 +0530

    without login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Pages
 import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
+// import Login from './pages/Login';
 import Orders from './pages/orders/Orders';
 import Review from './pages/Review/Review';
 
@@ -16,8 +16,11 @@ import DashCompo from "./pages/DashCompo";
 import Addbook from "./pages/Book/Addbook/Addbook";
 import Viewbook from "./pages/Book/ViewBook/Viewbook";
 
-// delete api 
-import ForgotPassword from "./pages/forgotPassword/ForgotPassword";
+// to show the pop mssg 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// import ForgotPassword from "./pages/forgotPassword/ForgotPassword";
 
 
 
@@ -25,8 +28,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgotPassword" element={<ForgotPassword/>}/>
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/forgotPassword" element={<ForgotPassword/>}/> */}
         <Route path="/" element={<Dashboard />}>
           {/* <Route path="add-inventory" element={<AddInventory />} /> */}
           {/* <Route path="inventory-list" element={<InventoryList />} /> */}
@@ -43,7 +46,8 @@ function App() {
           <Route path="/Viewbook" element={<Viewbook/>} />
       
           <Route path="/Orders" element={<Orders />} />
-          <Route path="/Review" element={<Review />} />
+          <Route path ="/Review"element ={<Review/>}/>
+        
           {/* <Route path="PurchaseHistory" element={<PurchaseHistory />} /> */}
          
           
